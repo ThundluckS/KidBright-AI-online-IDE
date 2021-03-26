7d82ff70f6c48d789f73288f8c828be1310e6499
Kobkrit Viriyayudhakorn <kobkrit@iapp.co.th>
2021-03-26 21:53:08 +0700

    Fix various 2

--- src/components/anonate.vue
+++ src/components/anonate.vue
@@ -538,7 +538,7 @@ export default {
         // array empty or does not exist
       } else {
         var img = new Image();
-        img.src = this.imageSrc();
+        img.src = "server/"+this.imageSrc();
 
         img.onload = function () {
           // here you got the width and height
